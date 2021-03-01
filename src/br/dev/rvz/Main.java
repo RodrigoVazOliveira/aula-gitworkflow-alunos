404d9a774b0f16fe49ef964dc238a01906e06f1b
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-01 14:34:07 -0300

    imprimir alunos

--- src/br/dev/rvz/Main.java
+++ src/br/dev/rvz/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        for (Aluno aluno : Aluno.values()) {
+            System.out.println(aluno);
+        }
     }
 }
